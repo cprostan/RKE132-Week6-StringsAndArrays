a61a5f5e4ec58b4cba6fc977d42b186986233ac3
Cristo Prostang <cprostan@tktk.ee>
2022-11-01 19:00:12 +0200

    add Week6StringsAndArrays code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int RandomIndex = rnd.Next(snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[RandomIndex]}");
